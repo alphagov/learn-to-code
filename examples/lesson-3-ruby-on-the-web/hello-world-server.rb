4ffa1395f917990bbf2cfa81039ae2587f891f6f
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2019-05-29 21:46:38 +0100

    WIP

---
+++ examples/lesson-3-ruby-on-the-web/hello-world-server.rb
@@ -0,0 +1,9 @@
+require("webrick")
+server = WEBrick::HTTPServer.new(Port: 8080, DocumentRoot: './public')
+
+server.mount_proc("/") do |request, response|
+  puts("Hello server!")
+  response.body = "Hello browser!"
+end
+
+server.start()
