01b5b75c51e0b0ac790e4fd8e4ca5aa111049d74
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2019-05-30 12:22:05 +0100

    Add Answers to Tasks 7 & 8

--- examples/lesson-3-ruby-on-the-web/barking-permit-server.rb
+++ examples/lesson-3-ruby-on-the-web/barking-permit-server.rb
@@ -2,7 +2,16 @@ require("webrick")
 server = WEBrick::HTTPServer.new(Port: 8080, DocumentRoot: './public')
 
 server.mount_proc("/home") do |request, response|
-  response.body = File.read("index.html")
+  dog_name = request.query["dog-name"]
+  dog_age = request.query["dog-age"]
+
+  if dog_name
+    response.body = File.read("barking-permit-template.html").
+      sub("DOG_NAME", dog_name).
+      sub("DOG_AGE_HUMAN_YEARS", dog_age)
+  else
+    response.body = File.read("index.html")
+  end
 end
 
 server.start()
