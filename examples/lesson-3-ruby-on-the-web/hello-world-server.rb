a483c48ff12edfdaf2408da0fe4d4764ac1fd564
Murilo Dal Ri <murilodalri@gmail.com>
2024-01-11 12:06:49 +0000

    Remove example page
    The example page was also hosted on PaaS and is an actual Ruby server.
    It's more difficult to host and not essential to the course, so we decided to remove it.

--- examples/lesson-3-ruby-on-the-web/hello-world-server.rb
+++
@@ -1,9 +0,0 @@
-require("webrick")
-server = WEBrick::HTTPServer.new(Port: 8080, DocumentRoot: './public')
-
-server.mount_proc("/") do |request, response|
-  puts("Hello server!")
-  response.body = "Hello browser!"
-end
-
-server.start
